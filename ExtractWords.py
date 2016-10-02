4ed25c4dfb61d08007c38783e9891797d024afd9
tusizi <529340225@qq.com>
2016-10-02 16:32:29 +0000

    add tf idf

--- ExtractWords.py
+++ ExtractWords.py
@@ -2,17 +2,18 @@
 
 import json
 
-import jieba
+from jieba import analyse
 from pyspark import SparkContext
 
 sc = SparkContext(appName='DocumentAnalysis')
 
 
 def cut(entity):
-    seg_list = jieba.cut(entity[1], cut_all=True)
+    # seg_list = jieba.cut(entity[1], cut_all=True)
+    seg_list = analyse.extract_tags(entity[1], 20, True)
     filename = "/vagrant/word/160930/" + entity[0]
-    filtered_list = filter(lambda x: x != "", seg_list)
-    r = ",".join(filtered_list)
+    filtered_list = filter(lambda (x, y): x != "", seg_list)
+    r = json.dumps(filtered_list)
     fo = open(filename, "a+")
     fo.write(r.encode("utf-8"))
     fo.close()
