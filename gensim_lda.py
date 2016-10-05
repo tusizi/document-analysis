2236a5d849eaf692d114cdd9db32aa15995a171c
tusizi <529340225@qq.com>
2016-10-05 10:17:34 +0000

    rename all files

---
+++ gensim_lda.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import gensim
+
+from gensim import corpora
+
+documents = ["Human machine interface for lab abc computer applications",
+             "A survey of user opinion of computer system response time",
+             "The EPS user interface management system",
+             "System and human system engineering testing of EPS",
+             "Relation of user perceived response time to error measurement",
+             "The generation of random binary unordered trees",
+             "The intersection graph of paths in trees",
+             "Graph minors IV Widths of trees and well quasi ordering",
+             "Graph minors A survey"]
+
+texts = [[word for word in document.lower().split()] for document in documents]
+
+dictionary = corpora.Dictionary(texts)
+corpus = [dictionary.doc2bow(text) for text in texts]
+
+lda_result = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=3)
+
+lda_result.print_topics(2)
